4eab74bab6b8d995f8f7b8ecf09c745260204f95
Jhannet <jhannet.jancko.camacho@gmail.com>
2016-07-07 12:37:21 -0400

    sin modal

--- tis/src/EvaluationsBundle/Form/QuestionType.php
+++ tis/src/EvaluationsBundle/Form/QuestionType.php
@@ -21,19 +21,6 @@ class QuestionType extends AbstractType
         $builder
             ->add('statementQuestion')
             ->add('pathImageQuestion')
-<<<<<<< HEAD
-            ->add('pathFileQuestion')
-            ->add('idType',EntityType::class, array(
-                    'class' => 'EvaluationsBundle:TypeQuestion','query_builder' => function (EntityRepository $er) {
-                        return $er->createQueryBuilder('u');
-                    },
-                    'choice_label' => 'nameType',
-                    'choice_value' => 'idType',
-                    'required' => true,
-                ))
-            ->add('idType', HiddenType::class)
-=======
->>>>>>> bbffbfa7e3f77b70b313d01192e2f223cbc43c2d
             ->add('area', EntityType::class, array(
                     'class' => 'EvaluationsBundle:Area',
                     'query_builder' => function (EntityRepository $er) {
