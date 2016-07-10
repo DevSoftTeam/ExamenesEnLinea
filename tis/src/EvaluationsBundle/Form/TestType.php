a07aa853312b2772b434f6060b21478cb9918002
irene <irene_airene@hotmail.com>
2016-07-10 13:10:27 -0400

    vista new test registra hora y ambien fecha

--- tis/src/EvaluationsBundle/Form/TestType.php
+++ tis/src/EvaluationsBundle/Form/TestType.php
@@ -21,19 +21,20 @@ class TestType extends AbstractType
             ->add('institution')
             ->add('startTime', TimeType::class, array(
                 'widget' => 'single_text',
-                'attr' => ['type' => 'time'],
+//                'attr' => ['type' => 'time'],
+//                'format' => 'HH:ii',
                 'label' => ' ',
                 'required' =>false
             ))
             ->add('startDate',DateType::class, array(
                 'widget' => 'single_text',
-                'format' => 'yyyy-MM    -dd',
+                'format' => 'yyyy-MM-dd',
                 'attr' => ['class' => 'datepicker'],
                 'required' => false,
             ))
             ->add('endTime', TimeType::class, array(
                 'widget' => 'single_text',
-                'attr' => ['type' => 'time'],
+//                'attr' => ['type' => 'time'],
                 'label' => ' ',
                 'required' =>false
             ))
@@ -45,7 +46,7 @@ class TestType extends AbstractType
             ))
             ->add('startTimeEnrollment', TimeType::class, array(
                 'widget' => 'single_text',
-                'attr' => ['type' => 'time'],
+//                'attr' => ['type' => 'time'],
                 'label' => ' ',
                 'required' =>false
             ))
@@ -57,7 +58,7 @@ class TestType extends AbstractType
             ))
             ->add('endTimeEnrollment', TimeType::class, array(
                 'widget' => 'single_text',
-                'attr' => ['type' => 'time'],
+//                'attr' => ['type' => 'time'],
                 'label' => ' ',
                 'required' =>false
             ))
