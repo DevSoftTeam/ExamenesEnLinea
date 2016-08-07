7b7a7e8cb587484462654369eb5eccef5b09eaf7
Irene Argollo Delgadillo <irene_airene@hotmail.com>
2016-08-07 12:02:48 -0400

    there are task taken and user to show the payroll qualification

--- tis/src/EvaluationsBundle/Controller/PayrollQualificationsController.php
+++ tis/src/EvaluationsBundle/Controller/PayrollQualificationsController.php
@@ -5,11 +5,16 @@ namespace EvaluationsBundle\Controller;
 
 class PayrollQualificationsController
 {
-    public function showPayrollQualificationsAction() {
-        return $this->render('area/show.html.twig', array(
-            'area' => $area,
-            'delete_form' => $deleteForm->createView(),
+    public function showPayrollQualificationsAction($idTest) {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+        $testTakens = $em->getRepository('EvaluationsBundle:TestTaken')->findBy(array('idTest'=>$idTest));
+        $userAnswer = $em->getRepository('EvaluationsBundle:UserAnswer')->findBy(array('idTest'=>$idTest));
+        return $this->render('EvaluationsBundle:TestForm:payrollQualifications.html.twig', array(
+            'testTaken' => $testTakens,
+            'userAnswewer'=>$userAnswer
         ));
+
     }
 
     public function newPayrollQualificationsAction() {
