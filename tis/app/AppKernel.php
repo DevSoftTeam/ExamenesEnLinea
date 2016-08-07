c28c12e27dca383b00dcdfbe01e7a007a935fb46
Irene Argollo Delgadillo <irene_airene@hotmail.com>
2016-08-07 10:04:25 -0400

    dompdf is delete on the app kernel

--- tis/app/AppKernel.php
+++ tis/app/AppKernel.php
@@ -18,8 +18,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new EvaluationsBundle\EvaluationsBundle(),
-            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
+            new EvaluationsBundle\EvaluationsBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
