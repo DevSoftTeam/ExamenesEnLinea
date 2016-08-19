149bb5511ed95141ef806ed54758fb03e20f0764
Jhannet <jhannet.jancko.camacho@gmail.com>
2016-08-19 10:37:16 -0400

    mostrar pregunta complecion en test scoreTest y asignscore2

--- tis/src/EvaluationsBundle/Entity/Question.php
+++ tis/src/EvaluationsBundle/Entity/Question.php
@@ -143,6 +143,13 @@ class Question
         return $html;
     }
 
+    public function getCompletionStatement()
+    {
+        $statement = json_decode($this->statementQuestion,true);
+        $statement = $statement["show"];
+        return $statement;
+    }
+
     public function getViewStatement(){
         $html = "";
         if ($this->pathImageQuestion != null) {
